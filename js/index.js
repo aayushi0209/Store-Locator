118d0f1d38b6241aba187ce36eb5a58f696291cc
Aayushi Gupta <aayushigupta0209@gmail.com>
2020-05-29 19:07:54 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,403 @@
+
+window.onload=()=>{
+  displayStores();
+}
+
+
+var map;
+var markers = [];
+var infoWindow;
+
+function initMap() {
+
+
+  var styledMapType=new google.maps.StyledMapType(
+    [
+      {
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#1d2c4d"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#8ec3b9"
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1a3646"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.country",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#4b6878"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.land_parcel",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#64779e"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.province",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#4b6878"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#334e87"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#023e58"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#283d6a"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#6f9ba5"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1d2c4d"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#023e58"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#3C7680"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#304a7d"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#98a5be"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1d2c4d"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#2c6675"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {
+            "color": "#255763"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#b0d5ce"
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#023e58"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#98a5be"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "color": "#1d2c4d"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {
+            "color": "#283d6a"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#3a4762"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "color": "#0e1626"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#4e6d70"
+          }
+        ]
+      }
+    ],{name:'Styled Map'}
+  )
+
+
+    var losAngeles = {
+        lat: 34.063380,
+         lng: -118.358080
+      }
+    
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: losAngeles,
+      zoom: 8,
+      mapTypeControlOptions: {
+        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'styled_map']
+      }
+    });
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
+   infoWindow = new google.maps.InfoWindow();
+
+    // displayStores()
+    // showStoresMarkers()
+        searchStores();
+
+    
+  }
+
+function searchStores(){
+    var foundStores = [];
+    var zipCode = document.getElementById('zip-code-input').value;
+    if(zipCode){
+        stores.forEach(function(store){
+            var postal = store.address.postalCode.substring(0,5);
+            if(postal == zipCode){
+                foundStores.push(store);
+            }
+        });
+    } 
+    else {
+        foundStores = stores;
+    }
+    clearLocations()
+    displayStores(foundStores);
+    showStoresMarkers(foundStores);
+    setOnClickListener();
+}
+
+
+function clearLocations() {
+    infoWindow.close();
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+}
+
+
+function setOnClickListener() {
+    var storeElements = document.querySelectorAll('.store-container');
+    storeElements.forEach(function(elem, index){
+        elem.addEventListener('click', function(){
+            google.maps.event.trigger(markers[index], 'click');
+        })
+    });
+}
+
+function displayStores(stores) {
+    var storesHtml = "";
+    stores.forEach(function(store, index){
+        var address = store.addressLines;
+        var phone = store.phoneNumber;
+        storesHtml += `
+        <button id=index onclick="search(${index})">
+
+             <div class="store-container">
+                 <div class="store-info-container">
+                     <div class="store-address">
+                         <span>${address[0]}</span>
+                         <span>${address[1]}</span>
+                     </div>
+                     <div class="store-phone-number">${phone}</div>
+                 </div>
+                 <div class="store-number-container">
+                     <div class="store-number">
+                         ${index+1}
+                     </div>
+                 </div>
+             </div>
+          </button>
+        <hr>
+        `
+    });
+    document.querySelector('.stores-list').innerHTML = storesHtml;
+}
+
+
+function showStoresMarkers(stores) {
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function(store, index){
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
+        var name = store.name;
+        var address = store.addressLines[0];
+        var statusText = store.openStatusText;
+        var phone = store.phoneNumber;
+        bounds.extend(latlng);
+        createMarker(latlng, name, address, statusText, phone, index);
+    })
+    map.fitBounds(bounds);
+}
+
+
+function createMarker(latlng, name, address, statusText, phone, index) {
+    var html = `
+        <div class="store-info-window">
+            <div class="store-info-name">
+                ${name}
+            </div>
+
+            <div class="store-info-status">
+                ${statusText}
+            </div>
+            <div class="store-info-address">
+                <div class="circle">
+                    <i class="fas fa-location-arrow"></i>
+                </div>
+                ${address}
+            </div>
+            <div class="store-info-phone">
+                <div class="circle">
+                    <i class="fas fa-phone-alt"></i>
+                </div>
+                ${phone}
+            </div>
+        </div>
+    `;
+    var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      label: `${index+1}`
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.setContent(html);
+      infoWindow.open(map, marker);
+    });
+    markers.push(marker);
+}
+
+
+
+
+
---
+++ todo.md
@@ -0,0 +1,15 @@
+# Plan Of Action
+
+-create wireframe DONE
+
+-set up base html structure DONE
+
+-Import google maps into the window
+
+-add text locator
+
+-add input box
+
+-add store list container 
+
+-add individual store DONE
